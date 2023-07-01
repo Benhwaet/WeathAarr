f0d9b55595ba112e9c4a85bf3f36fea9959c337e
BenHwaet <benmdesroches@gmail.com>
2023-07-01 19:03:41 -0300

    All data is now being fetched without error

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,17 @@ function getCityName() {
       var latitude = data[0].lat;
       var longitude = data[0].lon;
 
-      // var lat = latitude.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0];
-      // var lon = longitude.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0];
-      console.log(latitude, longitude);
-      getWeathArr(latitude, longitude);
+      var lat = latitude.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0];
+      var lon = longitude.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0];
+      console.log(lat, lon);
+      getWeathArr(lat, lon);
     })
+   
 };
 
 function getWeathArr(latitude, longitude) {
-  var getWeather = `api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=a58950883dae614383cd128e22972f9c`;
+
+  var getWeather = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=a58950883dae614383cd128e22972f9c&units=metric`;
 
   fetch(getWeather)
     .then(function (res) {
@@ -31,15 +33,5 @@ function getWeathArr(latitude, longitude) {
     .then(function (data) {
       console.log(data)
     })
-}
+};
 
-
-//  if (cityName) {
-//     // This will run and return to the homepage if there was nothing in the URL query parameter.
-//     cityName.textContent = '';
-//     location.assign('./index.html');
-//   } else {
-
-//     getWeather(cityName);
-//   }
-// };
